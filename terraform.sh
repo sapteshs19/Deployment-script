7f5bc61c19ebd635ffea96a6ee57b347a6285570
Saptesh <58178279+sapteshs19@users.noreply.github.com>
2025-03-25 03:56:02 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,4 +1,4 @@
 sudo wget https://releases.hashicorp.com/terraform/1.11.2/terraform_1.11.2_linux_amd64.zip
 apt install unzip
-unzip terraform.zip -d /usr/local/bin/
+unzip terraform_1.11.2_linux_amd64.zip -d /usr/local/bin/
 rm terraform.zip
